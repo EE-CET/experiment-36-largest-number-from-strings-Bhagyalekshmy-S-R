3616c15decf08caafb30a2420340dce04f91f97a
Your Name <you@example.com>
2026-02-06 09:23:55 +0530

    first commit

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -4,11 +4,45 @@ import java.util.Comparator;
 
 public class LargestNumber {
    
+        
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         // TODO: Read N
+        int n = sc.nextInt();
+
         // TODO: Read N strings into an array
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
         // TODO: Sort the strings using a custom comparator
         // Comparator logic: (str1, str2) -> (str2 + str1).compareTo(str1 + str2)
-        // TODO: Handle edge case where all numbers are "0" (e.g. input 0 0 -> output "0")
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String str1, String str2) {
+                return (str2 + str1).compareTo(str1 + str2);
+            }
+        });
+
+        // TODO: Handle edge case where all numbers are "0"
+        if (arr[0].equals("0")) {
+            System.out.println("0");
+            return;
+        }
+
         // TODO: Concatenate sorted strings and print
-    
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
+
+        System.out.println(result.toString());
+        sc.close();
+    }
 }
+
+    
+
